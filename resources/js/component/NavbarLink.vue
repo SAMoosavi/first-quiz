bd411a211847a0ee82c7f0396cc598530ca9a4d5
Ali.Moosavi <moosavi238@gmail.com>
2022-01-01 10:06:25 +0330

    creat component navbar link

---
+++ resources/js/component/NavbarLink.vue
@@ -0,0 +1,17 @@
+<template>
+    <Link :href="href" class="mx-4 text-gray-200 dark:text-gray-700">
+        <slot></slot>
+    </Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    components: {
+        Link,
+    },
+    props: ["href"],
+};
+</script>
+
+<style></style>
