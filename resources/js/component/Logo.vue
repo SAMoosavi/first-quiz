21ab51410117a02518f7e4b277def7bf14d0c71b
Ali.Moosavi <moosavi238@gmail.com>
2022-01-01 10:06:45 +0330

    creat logo component

---
+++ resources/js/component/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+    <p class="text-red-800 dark:text-red-500">
+        <span class="text-base"> سامانه </span>
+        <span class="text-lg text-gray-800 dark:text-gray-500"> آزمون </span>
+        <span class="text-base"> آنلاین </span>
+    </p>
+</template>
