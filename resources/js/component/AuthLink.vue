730808418c1cb46b28e992f94d564a9fa81cc74f
Ali.Moosavi <moosavi238@gmail.com>
2022-01-02 21:13:09 +0330

    creat auth link

---
+++ resources/js/component/AuthLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <Link
+        :href="href"
+        class="text-sm font-medium text-gray-400 underline  dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+    >
+        <slot />
+    </Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    components: {
+        Link,
+    },
+    props: ["href"],
+};
+</script>
+
+<style></style>
