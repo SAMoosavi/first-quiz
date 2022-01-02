b9bf1d71cdb5e4773ed141d0ac2b297aacabb88f
Ali.Moosavi <moosavi238@gmail.com>
2022-01-02 21:05:22 +0330

    customis forgot password page

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,16 +1,22 @@
 <template>
-    <Head title="Forgot Password" />
+    <Head title="فراموشی رمز عبور" />
 
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
+    <authentication-card>
+        <template #img>
+            <img
+                :src="'../image/Forgot-Password.png'"
+                class="w-full max-h-screen"
+                alt="تصویر صفحه فراموشی رمز عبور"
+            />
         </template>
 
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+        <div class="my-4 text-sm font-medium text-justify text-gray-700 dark:text-gray-200">
+            رمز عبور خود را فراموش کرده اید؟ مشکلی نیست فقط آدرس ایمیل خود را به
+            ما بگویید تا ما یک پیوند بازنشانی رمز عبور را برای شما ایمیل می کنیم
+            که به شما امکان می دهد رمز جدیدی را انتخاب کنید.
         </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
             {{ status }}
         </div>
 
@@ -18,56 +24,74 @@
 
         <form @submit.prevent="submit">
             <div>
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+                <Label for="email" value="ایمیل" />
+                <Input
+                    id="email"
+                    type="email"
+                    class="block w-full mt-1"
+                    v-model="form.email"
+                    required
+                    autofocus
+                />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </jet-button>
+                <Button
+                    class="mr-4"
+                    :loding="loding"
+                    :disabled="form.processing"
+                >
+                    ارسال ایمیل
+                </Button>
             </div>
         </form>
-    </jet-authentication-card>
+    </authentication-card>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import { Head } from '@inertiajs/inertia-vue3';
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
+import {  ref } from "vue";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
+import AuthenticationCard from "@/component/AuthenticationCard.vue";
+import Button from "@/component/Button.vue";
+import Input from "@/component/Input.vue";
+import Label from "@/component/Label.vue";
+import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
 
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetLabel,
-            JetValidationErrors
-        },
+export default {
+    components: {
+        Head,
+        AuthenticationCard,
+        Button,
+        Input,
+        Label,
+        JetValidationErrors,
+    },
 
-        props: {
-            status: String
-        },
+    props: {
+        status: String,
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    email: ''
-                })
-            }
-        },
+    setup() {
+        const form = useForm({
+            email: "",
+        });
 
-        methods: {
-            submit() {
-                this.form.post(this.route('password.email'))
-            }
+        const loding = ref(false);
+
+        function submit() {
+            loding.value = true;
+            this.form.post(this.route("password.email"), {
+                onFinish: () => {
+                    loding.value = false;
+                },
+            });
         }
-    })
+
+        return {
+            form,
+            submit,
+            loding,
+        };
+    },
+};
 </script>
