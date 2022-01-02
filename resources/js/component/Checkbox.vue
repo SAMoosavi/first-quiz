a9aeefbe0d7968f8936a136affcc9430240b214a
Ali.Moosavi <moosavi238@gmail.com>
2022-01-02 11:26:13 +0330

    edit Checkbox and cheng location

--- resources/js/component/Checkbox.vue
+++ resources/js/component/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <input type="checkbox" :value="value" v-model="proxyChecked"
-           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+           class="text-black border-none focus:border-indigo-300 focus:ring-0 ">
 </template>
 
 <script>
