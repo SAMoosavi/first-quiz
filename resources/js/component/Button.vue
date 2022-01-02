0a970f902b86c0d616925e6020af9327de9783aa
Ali.Moosavi <moosavi238@gmail.com>
2022-01-02 11:58:55 +0330

    edite button and cheng location

---
+++ resources/js/component/Button.vue
@@ -0,0 +1,51 @@
+<template>
+    <button
+        v-if="!loding"
+        :type="type"
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-black hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-0"
+    >
+        <slot></slot>
+    </button>
+    <button
+        v-else
+        disabled
+        :type="type"
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-gray-300 uppercase transition bg-gray-700 "
+    >
+        <slot></slot>
+        <svg
+            class="w-4 h-4 ml-1 mr-3 text-white animate-spin"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                class="opacity-50"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+            ></circle>
+            <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+        </svg>
+    </button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        type: {
+            type: String,
+            default: "submit",
+        },
+        loding: Boolean,
+    },
+});
+</script>
