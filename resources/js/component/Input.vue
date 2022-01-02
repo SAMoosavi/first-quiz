33b74003c3abb5c38ea7aad08fadb86ee63210f5
Ali.Moosavi <moosavi238@gmail.com>
2022-01-02 11:15:47 +0330

    edite and cheng location of Input

---
+++ resources/js/component/Input.vue
@@ -0,0 +1,24 @@
+<template>
+    <input
+        class="border-none  focus:ring focus:ring-sky-400 dark:focus:ring-sky-300 focus:ring-opacity-30 dark:focus:ring-opacity-80"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        ref="input"
+    />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: ["modelValue"],
+
+    emits: ["update:modelValue"],
+
+    methods: {
+        focus() {
+            this.$refs.input.focus();
+        },
+    },
+});
+</script>
