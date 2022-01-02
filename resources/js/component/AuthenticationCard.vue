3499f3c6bac0f71afcad30a56ab915addd8e3d75
Ali.Moosavi <moosavi238@gmail.com>
2022-01-02 10:48:10 +0330

    edit auth layout and cheng location

---
+++ resources/js/component/AuthenticationCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div
+        class="flex items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-sky-200 dark:bg-gray-900"
+    >
+        <div
+            class="w-full px-6 py-4 overflow-hidden sm:max-w-md bg-sky-50 dark:bg-sky-800 md:basis-1/2 lg:basis-1/3 md:mx-4"
+        >
+            <div class="flex justify-end">
+                <on-off-dark-mode class="inline-block" />
+            </div>
+            <slot />
+        </div>
+        <div
+            class="items-center hidden w-full md:flex md:basis-1/2 lg:basis-2/3 sm:justify-center"
+        >
+            <slot name="img" />
+        </div>
+    </div>
+</template>
+
+<script>
+import OnOffDarkMode from "@/component/OnOffDarkMode.vue";
+export default {
+    components: {
+        OnOffDarkMode,
+    },
+};
+</script>
