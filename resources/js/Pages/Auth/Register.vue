c732f93f0865dddf003ac3e0c9713678af2db0ea
Ali.Moosavi <moosavi238@gmail.com>
2022-01-03 08:35:38 +0330

    costomis register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,101 +1,147 @@
 <template>
-    <Head title="Register" />
+    <Head title="ثبت نام" />
 
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
+    <authentication-card>
+        <template #img>
+            <img
+                :src="'../image/register-light.png'"
+                class="block w-full max-h-screen dark:hidden"
+                alt="تصویر صفحه ثبت نام"
+            />
+            <img
+                :src="'../image/register-dark.png'"
+                class="hidden w-full max-h-screen dark:block"
+                alt="تصویر صفحه ثبت نام"
+            />
         </template>
-
         <jet-validation-errors class="mb-4" />
 
         <form @submit.prevent="submit">
             <div>
-                <jet-label for="name" value="Name" />
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+                <Label for="name" value="نام" />
+                <Input
+                    id="name"
+                    type="text"
+                    class="block w-full mt-1"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
             </div>
 
             <div class="mt-4">
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required />
+                <Label for="email" value="ایمیل" />
+                <Input
+                    id="email"
+                    type="email"
+                    class="block w-full mt-1"
+                    v-model="form.email"
+                    required
+                />
             </div>
 
             <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
+                <Label for="password" value="رمز عبور" />
+                <Input
+                    id="password"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="mt-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+                <Label for="password_confirmation" value="تکرار رمز عبور" />
+                <Input
+                    id="password_confirmation"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
-            <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
-                <jet-label for="terms">
+            <!-- <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
+                <Label for="terms">
                     <div class="flex items-center">
-                        <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
+                        <Checkbox name="terms" id="terms" v-model:checked="form.terms" />
 
                         <div class="ml-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
+                            I agree to the <a target="_blank" :href="route('terms.show')" class="text-sm text-gray-600 underline hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="text-sm text-gray-600 underline hover:text-gray-900">Privacy Policy</a>
                         </div>
                     </div>
-                </jet-label>
-            </div>
+                </Label>
+            </div> -->
 
             <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
+                <auth-link :href="route('login')"> حسابی دارید! </auth-link>
 
-                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </jet-button>
+                <Button
+                    class="mr-4"
+                    :loding="loding"
+                    :disabled="form.processing"
+                >
+                    ثبت نام
+                </Button>
             </div>
         </form>
-    </jet-authentication-card>
+    </authentication-card>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetCheckbox from '@/Jetstream/Checkbox.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
+import { defineComponent, ref } from "vue";
+import AuthenticationCard from "@/component/AuthenticationCard.vue";
+import JetAuthenticationCardLogo from "@/Jetstream/AuthenticationCardLogo.vue";
+import Button from "@/component/Button.vue";
+import Input from "@/component/Input.vue";
+import Checkbox from "@/component/Checkbox.vue";
+import Label from "@/component/Label.vue";
+import AuthLink from "@/component/AuthLink.vue";
+import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
 
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetCheckbox,
-            JetLabel,
-            JetValidationErrors,
-            Link,
-        },
+export default defineComponent({
+    components: {
+        Head,
+        AuthenticationCard,
+        JetAuthenticationCardLogo,
+        Button,
+        Input,
+        Checkbox,
+        Label,
+        JetValidationErrors,
+        AuthLink,
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    name: '',
-                    email: '',
-                    password: '',
-                    password_confirmation: '',
-                    terms: false,
-                })
-            }
-        },
+    setup() {
+        const form = useForm({
+            name: "",
+            email: "",
+            password: "",
+            password_confirmation: "",
+            terms: false,
+        });
+        const loding = ref(false);
 
-        methods: {
-            submit() {
-                this.form.post(this.route('register'), {
-                    onFinish: () => this.form.reset('password', 'password_confirmation'),
-                })
-            }
+        function submit() {
+            loding.value = true;
+            form.post(this.route("register"), {
+                onFinish: () => {
+                    loding.value = false;
+                    form.reset("password", "password_confirmation");
+                },
+            });
         }
-    })
+
+        return {
+            form,
+            submit,
+            loding,
+        };
+    },
+});
 </script>
