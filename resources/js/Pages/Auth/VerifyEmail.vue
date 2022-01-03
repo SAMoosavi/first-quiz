9cdda80512839ce45ee05ac7944cc9f0bd45a16f
Ali.Moosavi <moosavi238@gmail.com>
2022-01-03 09:08:08 +0330

    customis verify email

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,67 +1,97 @@
 <template>
     <Head title="Email Verification" />
 
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
+    <authentication-card>
+        <template #img>
+            <img
+                :src="'../image/verify-email.png'"
+                class="w-full max-h-screen"
+                alt="تصویر صفحه تایید ایمیل"
+            />
         </template>
 
-        <div class="mb-4 text-sm text-gray-600">
-            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+        <div
+            class="
+                my-4
+                text-sm
+                font-medium
+                text-justify text-gray-700
+                dark:text-gray-200
+            "
+        >
+            از ثبت نام شما سپاسگزاریم! قبل از شروع، آیا می توانید آدرس ایمیل خود
+            را با کلیک بر روی پیوندی که به تازگی برای شما ایمیل کرده ایم تأیید
+            کنید؟ اگر ایمیلی را دریافت نکردید، با کمال میل یک ایمیل دیگر برای
+            شما ارسال خواهیم کرد.
         </div>
 
-        <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent" >
-            A new verification link has been sent to the email address you provided during registration.
+        <div
+            class="mb-4 text-sm font-medium text-green-600"
+            v-if="verificationLinkSent"
+        >
+            یک پیوند تأیید جدید به آدرس ایمیلی که در هنگام ثبت نام ارائه کرده
+            اید ارسال شده است.
         </div>
 
         <form @submit.prevent="submit">
-            <div class="mt-4 flex items-center justify-between">
-                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Resend Verification Email
-                </jet-button>
-
-                <Link :href="route('logout')" method="post" as="button" class="underline text-sm text-gray-600 hover:text-gray-900">Log Out</Link>
+            <div class="flex items-center justify-between mt-4">
+                <Button
+                    class="mr-4"
+                    :loding="loding"
+                    :disabled="form.processing"
+                >
+                    ایمیل تایید را دوباره بفرست
+                </Button>
+                <auth-link :href="route('logout')" :method="'post'" as="button">
+                    Log Out
+                </auth-link>
             </div>
         </form>
-    </jet-authentication-card>
+    </authentication-card>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
+import { ref } from "vue";
+import AuthenticationCard from "@/component/AuthenticationCard.vue";
+import Button from "@/component/Button.vue";
+import AuthLink from "@/component/AuthLink.vue";
+import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            Link,
-        },
+export default {
+    components: {
+        Head,
+        AuthenticationCard,
+        Button,
+        Link,
+        AuthLink,
+    },
 
-        props: {
-            status: String
-        },
+    props: {
+        status: String,
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form()
-            }
-        },
+    setup(props) {
+        const form = useForm();
+        const loding = ref(false);
 
-        methods: {
-            submit() {
-                this.form.post(this.route('verification.send'))
-            },
-        },
-
-        computed: {
-            verificationLinkSent() {
-                return this.status === 'verification-link-sent';
-            }
+        function submit() {
+            loding.value = true;
+            form.post(this.route("verification.send"), {
+                onFinish: () => {
+                    loding.value = false;
+                },
+            });
         }
-    })
+
+        function verificationLinkSent() {
+            return props.status === "verification-link-sent";
+        }
+
+        return {
+            submit,
+            verificationLinkSent,
+            loding,
+        };
+    },
+};
 </script>
