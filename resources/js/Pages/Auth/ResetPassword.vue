868693595aea18e7217a65ba243042bf5af39e44
Ali.Moosavi <moosavi238@gmail.com>
2022-01-03 08:53:39 +0330

    costomis reset pass

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,81 +1,114 @@
 <template>
-    <Head title="Reset Password" />
+    <Head title="بازیابی رمز عبور" />
 
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
+    <authentication-card>
+        <template #img>
+            <img
+                :src="'../image/reset-password.png'"
+                class="w-full max-h-screen"
+                alt="تصویر صفحه بازیابی رمز عبور"
+            />
         </template>
 
         <jet-validation-errors class="mb-4" />
 
         <form @submit.prevent="submit">
             <div>
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+                <Label for="email" value="ایمیل" />
+                <Input
+                    id="email"
+                    type="email"
+                    class="block w-full mt-1"
+                    v-model="form.email"
+                    required
+                    autofocus
+                />
             </div>
 
             <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
+                <Label for="password" value="رمز عبور" />
+                <Input
+                    id="password"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="mt-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+                <Label for="password_confirmation" value="تکرار رمز عبور" />
+                <Input
+                    id="password_confirmation"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
-                </jet-button>
+                <Button
+                    class="mr-4"
+                    :loding="loding"
+                    :disabled="form.processing"
+                >
+                    بازیابی رمز عبور
+                </Button>
             </div>
         </form>
-    </jet-authentication-card>
+    </authentication-card>
 </template>
 
 <script>
-    import { defineComponent } from 'vue';
-    import { Head } from '@inertiajs/inertia-vue3';
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
+import { ref } from "vue";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
+import AuthenticationCard from "@/component/AuthenticationCard.vue";
+import Button from "@/component/Button.vue";
+import Input from "@/component/Input.vue";
+import Label from "@/component/Label.vue";
+import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
 
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetLabel,
-            JetValidationErrors
-        },
+export default {
+    components: {
+        Head,
+        AuthenticationCard,
+        Button,
+        Input,
+        Label,
+        JetValidationErrors,
+    },
 
-        props: {
-            email: String,
-            token: String,
-        },
+    props: {
+        email: String,
+        token: String,
+    },
+    setup(props) {
+        const form = useForm({
+            token: props.token,
+            email: props.email,
+            password: "",
+            password_confirmation: "",
+        });
+        const loding = ref(false);
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    token: this.token,
-                    email: this.email,
-                    password: '',
-                    password_confirmation: '',
-                })
-            }
-        },
-
-        methods: {
-            submit() {
-                this.form.post(this.route('password.update'), {
-                    onFinish: () => this.form.reset('password', 'password_confirmation'),
-                })
-            }
+        function submit() {
+            loding.value = true;
+            form.post(this.route("password.update"), {
+                onFinish: () => {
+                    form.reset("password", "password_confirmation"),
+                        (loding.value = false);
+                },
+            });
         }
-    })
+
+        return {
+            form,
+            submit,
+            loding,
+        };
+    },
+};
 </script>
