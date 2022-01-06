5b45d2f8518d2f8478ba2b3f436e1ad339aae23b
Ali.Moosavi <moosavi238@gmail.com>
2022-01-06 09:54:37 +0330

    rename database

--- database/migrations/2021_12_31_121533_creat_user_quiz_table.php
+++ database/migrations/2021_12_31_121533_creat_user_quiz_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatUserQuizTable extends Migration
+class CreatQuizUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatUserQuizTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_quiz', function (Blueprint $table) {
+        Schema::create('quiz_user', function (Blueprint $table) {
             $table->foreignId('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreignId('quiz_id');
@@ -29,6 +29,6 @@ class CreatUserQuizTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_quiz');
+        Schema::dropIfExists('quiz_user');
     }
 }
