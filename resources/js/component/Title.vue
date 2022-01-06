ee63100baa1e7fc926e620d33986f5cb8e939c33
Ali.Moosavi <moosavi238@gmail.com>
2022-01-06 12:47:37 +0330

    creat title of dashboard

---
+++ resources/js/component/Title.vue
@@ -0,0 +1,5 @@
+<template>
+    <h3 class="text-center text-lg text-gray-600 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-50 pb-3">
+        <slot />
+    </h3>
+</template>
