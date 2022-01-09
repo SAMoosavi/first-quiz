f5755c4a5c67784e786503d8441adfbe56b5fbe8
Ali.Moosavi <moosavi238@gmail.com>
2022-01-09 11:45:15 +0330

    add required in label

--- resources/js/component/Label.vue
+++ resources/js/component/Label.vue
@@ -1,14 +1,35 @@
 <template>
-    <label class="block text-sm font-medium text-gray-700 dark:text-gray-200">
+    <label
+        :class="`block text-sm font-medium text-gray-700 dark:text-gray-200 ${required}`"
+    >
         <span v-if="value">{{ value }}</span>
         <span v-else><slot></slot></span>
     </label>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from "vue";
 
-    export default defineComponent({
-        props: ['value']
-    })
+export default defineComponent({
+    props: ["value", "required"],
+    setup(props) {
+        let required = ref("");
+        if (!!props.required) {
+            required.value =
+                "after:content-['*'] after:mr-0.5 after:text-red-500";
+        }
+        watch(
+            () => props.required,
+            () => {
+                if (!!props.required) {
+                    required.value =
+                        "after:content-['*'] after:mr-0.5 after:text-red-500";
+                }
+            }
+        );
+        return {
+            required,
+        };
+    },
+});
 </script>
