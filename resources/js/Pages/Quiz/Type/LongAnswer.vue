bf9d6ab53a6a64abdd3c7dcebcc40ac1e1c09fd5
Ali.Moosavi <moosavi238@gmail.com>
2022-01-15 09:48:23 +0330

    creat long answer

---
+++ resources/js/Pages/Quiz/Type/LongAnswer.vue
@@ -0,0 +1,16 @@
+<template>
+    <textarea  disabled rows="1" class="w-full my-3 bg-gray-300 border-transparent"></textarea>
+</template>
+<script>
+import { onMounted } from "@vue/runtime-core";
+export default {
+    setup(props,{emit}) {
+        onMounted(() => {
+            emit('option',null);
+            emit('answer',null);
+        });
+    },
+};
+</script>
+
+<style></style>
