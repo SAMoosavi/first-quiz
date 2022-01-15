bd3e82b2a8cc64cbe44fd30df2b03ac13c3758b2
Ali.Moosavi <moosavi238@gmail.com>
2022-01-15 10:00:40 +0330

    add short answer

---
+++ resources/js/Pages/Quiz/Type/ShortAnswer.vue
@@ -0,0 +1,18 @@
+<template>
+    <Input disabled class="box-content w-1/2 h-6 p-2 my-3 bg-gray-300 md:w-1/3" />
+</template>
+<script>
+import { onMounted } from "@vue/runtime-core";
+import Input from "@/component/Input.vue";
+export default {
+    components: { Input },
+    setup(props, { emit }) {
+        onMounted(() => {
+            emit("option", null);
+            emit("answer", null);
+        });
+    },
+};
+</script>
+
+<style></style>
