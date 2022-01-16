337d45dd2b862f89a2bf84faae6641aec8cdc917
Ali.Moosavi <moosavi238@gmail.com>
2022-01-16 11:59:18 +0330

    okey store

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
+use App\Models\Question;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+use function PHPSTORM_META\map;
 
 class QuizController extends Controller
 {
-    //
+    public function show($uuid)
+    {
+        $quiz = Quiz::where('uuid', '=', $uuid)->get();
+        dd( $quiz[0]->questions);
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+                'name' => "required|string",
+                'start' => 'nullable|string',
+                'end' => 'nullable|string',
+                'time' => 'nullable|string',
+            ]);
+
+        $user = Auth::user();
+
+        $quiz = Quiz::create([
+            'name' => $request->name,
+            'user_id' => $user->id,
+            'start' => $request->start,
+            'end' => $request->end,
+            'time' => $request->time,
+            'uuid' => (string)Str::uuid(),
+        ]);
+
+        foreach ($request->questions as $question) {
+            Question::create([
+                'questions' => $question['question'],
+                'type' => $question['type'],
+                'option' => $question['option'],
+                'answer' => $question['answer'],
+                'quiz_id' => $quiz->id,
+                'uuid' => (string)Str::uuid(),
+            ]);
+        }
+
+        return Redirect::route('show.quiz', ['uuid' => $quiz->uuid]);
+    }
 }
