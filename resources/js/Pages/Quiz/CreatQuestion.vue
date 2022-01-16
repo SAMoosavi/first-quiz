d9e7114585897d0d9623b67532da71d4e291782e
Ali.Moosavi <moosavi238@gmail.com>
2022-01-16 09:55:20 +0330

    add creat question

---
+++ resources/js/Pages/Quiz/CreatQuestion.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="md:flex md:justify-between">
+        <div class="w-full md:w-4/6 md:gap-2 lg:gap-4">
+            <Label :required="true" :value="'سوال'" />
+            <textarea
+                name="question"
+                id="question"
+                v-model.lazy="question.question"
+                class="w-full border-transparent"
+                rows="1"
+            ></textarea>
+        </div>
+        <div class="md:w-1/6">
+            <Label :required="true" :value="'نوع سوال'" />
+            <select
+                name="type"
+                id="type"
+                class="w-full border-transparent"
+                v-model="question.type"
+            >
+                <option value="test-answer">پاسخ تستی</option>
+                <option value="long-answer">پاسخ طولانی</option>
+                <option value="short-answer">پاسخ کوتاه</option>
+            </select>
+        </div>
+    </div>
+    <component
+        :is="question.type"
+        @option="getOption"
+        @answer="getAnswer"
+        :index="$props.index"
+    />
+</template>
+<script>
+import { reactive } from "@vue/reactivity";
+import Label from "@/component/Label.vue";
+import LongAnswer from "@/Pages/Quiz/Type/LongAnswer.vue";
+import ShortAnswer from "@/Pages/Quiz/Type/ShortAnswer.vue";
+import TestAnswer from "@/Pages/Quiz/Type/TestAnswer.vue";
+import { watch } from "@vue/runtime-core";
+import { useStore } from "vuex";
+
+export default {
+    components: {
+        Label,
+        LongAnswer,
+        ShortAnswer,
+        TestAnswer,
+    },
+
+    props: ["index"],
+
+    setup(props) {
+        const store = useStore();
+
+        const question = reactive({
+            type: "test-answer",
+            question: null,
+            option: {},
+            answer: {},
+        });
+
+            const index = props.index;
+        function getOption(val) {
+            question.option = val;
+            store.commit("addQuestions", {question, index});
+        }
+
+        function getAnswer(val) {
+            question.answer = val;
+            store.commit("addQuestions", {question, index});
+        }
+
+        watch(
+            () => question.question,
+            () => {
+                store.commit("addQuestions", {question, index});
+            }
+        );
+        watch(
+            () => question.type,
+            () => {
+                store.commit("addQuestions", {question, index});
+            }
+        );
+
+        return {
+            getOption,
+            getAnswer,
+            question,
+        };
+    },
+};
+</script>
+
+<style></style>
