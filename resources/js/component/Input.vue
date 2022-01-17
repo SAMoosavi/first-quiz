d4af01683c79a74e182a38cf73fdddf57d71e75b
Ali.Moosavi <moosavi238@gmail.com>
2022-01-17 20:03:58 +0330

    okey edite size

--- resources/js/component/Input.vue
+++ resources/js/component/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <input
-        class="box-content h-6 p-2 text-gray-900 bg-transparent border-indigo-700 rounded-md shadow-sm shadow-indigo-600 dark:shadow-indigo-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
+        class="p-2 text-gray-900 bg-transparent border-indigo-700 rounded-md shadow-sm  shadow-indigo-600 dark:shadow-indigo-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         :type="TYPE"
