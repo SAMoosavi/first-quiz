09f90a4a02cfe1d8cfc5e3ee38bf06fb2553c7b8
Ali.Moosavi <moosavi238@gmail.com>
2022-01-17 15:16:18 +0330

    add fucos and cheng color

--- resources/js/component/AuthLink.vue
+++ resources/js/component/AuthLink.vue
@@ -1,15 +1,8 @@
 <template>
     <Link
+        :method="method"
         :href="href"
-        class="
-            text-sm
-            font-medium
-            text-gray-400
-            underline
-            dark:text-gray-400
-            hover:text-gray-900
-            dark:hover:text-white
-        "
+        class="text-sm font-medium text-gray-400 underline decoration-dashed hover:decoration-solid focus:decoration-solid underline-offset-4 dark:text-gray-400 hover:text-gray-900 focus:text-gray-900 dark:focus:text-white dark:hover:text-white hover:decoration-purple-700 focus:decoration-purple-700 dark:hover:decoration-purple-400 dark:focus:decoration-purple-400"
     >
         <slot />
     </Link>
