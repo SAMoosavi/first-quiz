357c3db24e16bda21bffd4ac19cd3cb14f43527f
Ali.Moosavi <moosavi238@gmail.com>
2022-01-17 15:05:40 +0330

    edit color and ui

--- resources/js/component/AuthenticationCard.vue
+++ resources/js/component/AuthenticationCard.vue
@@ -1,9 +1,9 @@
 <template>
     <div
-        class="flex items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-sky-200 dark:bg-gray-900"
+        class="flex items-center min-h-screen pt-6 bg-indigo-200 sm:justify-center sm:pt-0 dark:bg-slate-900"
     >
         <div
-            class="w-full px-6 py-4 overflow-hidden sm:max-w-md bg-sky-50 dark:bg-sky-800 md:basis-1/2 lg:basis-1/3 md:mx-4"
+            class="w-full px-6 py-4 overflow-hidden bg-indigo-100 border border-indigo-300 rounded-lg shadow-md dark:shadow-lg shadow-indigo-500/50 dark:shadow-indigo-300/50 dark:border-indigo-500 sm:max-w-md dark:bg-indigo-700 md:basis-1/2 lg:basis-1/3 md:mx-4"
         >
             <div class="flex justify-end">
                 <on-off-dark-mode class="inline-block" />
