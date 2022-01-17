bba2e9d3eb2c0a91030c0e710d7b6ebfd09ed981
Ali.Moosavi <moosavi238@gmail.com>
2022-01-17 16:12:03 +0330

    cheng button and ui

--- resources/js/component/Button.vue
+++ resources/js/component/Button.vue
@@ -2,7 +2,7 @@
     <button
         v-if="!loding"
         :type="type"
-        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-black hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-0"
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white transition-all bg-indigo-800 border-2 border-indigo-500 rounded-md shadow dark:text-gray-900 dark:border-indigo-400 dark:bg-indigo-600 dark:hover:bg-indigo-400 hover:bg-indigo-600 active:bg-indigo-900 dark:active:bg-indigo-500 focus:outline-none focus:ring-2 dark:focus:ring-2 dark:focus:ring-indigo-400/50 focus:ring-indigo-700/50 active:pl-12 dark:active:pl-12 shadow-indigo-700/50 dark:shadow-indigo-200/50"
     >
         <slot></slot>
     </button>
@@ -10,11 +10,11 @@
         v-else
         disabled
         :type="type"
-        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-gray-300 uppercase transition bg-gray-700 "
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white transition-all bg-indigo-900 border-2 border-indigo-500 rounded-md outline-none dark:text-gray-900 dark:border-indigo-400 dark:bg-indigo-500 ring-2 dark:ring-indigo-400/50 ring-indigo-700/50 "
     >
         <slot></slot>
         <svg
-            class="w-4 h-4 ml-1 mr-3 text-white animate-spin"
+            class="w-4 h-4 ml-1 mr-3 text-white dark:text-gray-900 animate-spin"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
