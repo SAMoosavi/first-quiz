b2c7efa1ad07b9190e7c82c30aee86193da391ac
Ali.Moosavi <moosavi238@gmail.com>
2022-01-17 15:55:45 +0330

    cheng input color and edite ui

--- resources/js/component/Input.vue
+++ resources/js/component/Input.vue
@@ -1,9 +1,9 @@
 <template>
     <input
-        class="box-content h-6 p-2 border-none focus:ring focus:ring-sky-400 dark:focus:ring-sky-300 focus:ring-opacity-30 dark:focus:ring-opacity-80"
+        class="box-content h-6 p-2 text-gray-900 bg-transparent border-indigo-700 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
-        ref="input"
+        :type="TYPE"
     />
 </template>
 
@@ -11,14 +11,13 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    props: ["modelValue"],
-
+    props: ["modelValue", "type"],
     emits: ["update:modelValue"],
-
-    methods: {
-        focus() {
-            this.$refs.input.focus();
-        },
+    setup(props) {
+        const TYPE = props.type ? props.type : "text";
+        return {
+            TYPE,
+        };
     },
 });
 </script>
