af3cb1fb453a38ce0012e39049a933aa6737c8b3
Ali.Moosavi <moosavi238@gmail.com>
2022-01-18 19:24:14 +0330

    delet p

--- resources/js/component/CardText.vue
+++ resources/js/component/CardText.vue
@@ -1,6 +1,6 @@
 <template>
     <p
-        class="inline-block text-gray-600 dark:text-gray-300 group-hover:text-slate-800 dark:group-hover:text-gray-200 pr-2 pb-4"
+        class="inline-block text-gray-600 dark:text-gray-300 group-hover:text-slate-800 dark:group-hover:text-gray-200 "
     >
         <slot />
     </p>
