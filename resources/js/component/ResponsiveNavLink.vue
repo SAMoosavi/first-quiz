229dce691426faf9c95dc285e3a1254ff65eecf9
Ali.Moosavi <moosavi238@gmail.com>
2022-01-18 10:33:05 +0330

    okey respansiv nave link

--- resources/js/component/ResponsiveNavLink.vue
+++ resources/js/component/ResponsiveNavLink.vue
@@ -26,8 +26,8 @@ export default defineComponent({
 
     setup(props) {
         const classes = props.active
-            ? "block pl-3 pr-4 py-2 border-l-4 border-sky-400 dark:border-sky-50 text-base font-medium text-gray-200 dark:text-gray-700 bg-sky-800 dark:bg-sky-300 focus:outline-none focus:text-white dark:focus:text-black focus:bg-sky-800 dark:focus:bg-sky-100 focus:border-sky-700 dark:focus:border-sky-300 transition"
-            : "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-300 dark:text-gray-700 hover:text-gray-100 dark:hover:text-gray-900 hover:bg-sky-600 dark:hover:bg-sky-300 hover:border-sky-500 dark:hover:border-sky-100 focus:outline-none focus:text-white dark:focus:text-black focus:bg-sky-800 dark:focus:bg-sky-100 focus:border-sky-500 dark:focus:border-sky-50 transition";
+            ? "block pl-3 pr-4 py-2 border-l-4 border-indigo-800 dark:border-indigo-200 text-base font-medium text-gray-200 dark:text-gray-700 bg-indigo-400 dark:bg-indigo-500 focus:outline-none focus:text-white dark:focus:text-black focus:bg-indigo-800 dark:focus:bg-indigo-100 focus:border-indigo-700 dark:focus:border-indigo-300 transition"
+            : "block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-400 dark:text-gray-800 hover:text-gray-100 dark:hover:text-gray-900 hover:bg-indigo-400 dark:hover:bg-indigo-500 hover:border-indigo-800 dark:hover:border-indigo-200 focus:outline-none focus:text-white dark:focus:text-black focus:bg-indigo-800 dark:focus:bg-indigo-300 focus:border-indigo-800 dark:focus:border-indigo-100 transition";
 
         const button = props.as == "button" ? true : false;
         const a = props.as == "a" ? true : false;
