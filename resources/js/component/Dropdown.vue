b6796e143ece5309aadca7f7ebd34a1c511e5333
Ali.Moosavi <moosavi238@gmail.com>
2022-01-18 09:48:30 +0330

    ok drapdown

--- resources/js/component/Dropdown.vue
+++ resources/js/component/Dropdown.vue
@@ -5,27 +5,22 @@
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div
-            v-show="open"
-            class="fixed inset-0 z-40"
-            @click="open = false"
-        ></div>
+        <div v-if="open" class="fixed inset-0 z-40" @click="open = false"></div>
 
         <transition
             enter-active-class="transition ease-out duration-200"
-            enter-from-class="transform opacity-0 -translate-y-1/4"
+            enter-from-class="transform opacity-0 -translate-y-1/3"
             enter-to-class="transform opacity-100 translate-y-0"
             leave-active-class="transition ease-in duration-100"
             leave-from-class="transform opacity-100 translate-y-0"
             leave-to-class="transform opacity-0 -translate-y-1/4"
         >
             <div
-                v-show="open"
-                :class="`absolute z-50 mt-2 rounded-none w-${width} origin-top-${align} ${align}-0`"
-                style="display: none"
+                v-if="open"
+                :class="`absolute z-50 mt-2 border border-transparent overflow-hidden rounded-b-md w-${width} origin-top-${align} ${align}-0`"
                 @click="open = false"
             >
-                <div class="rounded-none py-1" :class="color">
+                <div class="rounded-none" :class="color">
                     <slot name="content"></slot>
                 </div>
             </div>
@@ -48,7 +43,7 @@ export default {
         },
         color: {
             color: String,
-            default: "bg-sky-600 dark:bg-sky-500",
+            default: "bg-indigo-600 dark:bg-indigo-700",
         },
     },
 
