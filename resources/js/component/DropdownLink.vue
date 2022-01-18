5790ef15277f45dfd911f3de6ba8b5d72c26cca4
Ali.Moosavi <moosavi238@gmail.com>
2022-01-18 10:00:46 +0330

    ok dropdown link

--- resources/js/component/DropdownLink.vue
+++ resources/js/component/DropdownLink.vue
@@ -27,11 +27,11 @@ export default {
         const button = props.as == "button" ? true : false;
         const a = props.as == "a" ? true : false;
         const classes =
-            "block px-4 py-2 text-sm leading-5 text-gray-200 dark:text-gray-700 hover:text-white hover:dark:text-black hover:bg-sky-500 hover:dark:bg-sky-600 focus:text-white focus:outline-none focus:dark:text-black transition";
+            "block px-4 py-2 text-sm leading-5 text-gray-200 dark:text-gray-400 hover:text-white hover:dark:text-black hover:bg-indigo-400 hover:dark:bg-indigo-500 focus:text-white focus:outline-none focus:dark:text-black transition";
 
         return {
-            button,
             a,
+            button,
             classes,
         };
     },
