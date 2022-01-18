da34b83c8728d6a81a6d8a48ef695176e70c84a8
Ali.Moosavi <moosavi238@gmail.com>
2022-01-18 19:26:54 +0330

    ok dashbord

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -5,15 +5,15 @@
         <div class="md:pt-8">
             <div class="mx-auto lg:max-w-7xl lg:px-8">
                 <div
-                    class="grid min-h-full grid-cols-1 gap-2 p-2 overflow-hidden border-indigo-500 shadow-xl md:border bg-indigo-300/70 dark:bg-indigo-500/70 sm:rounded-none lg:rounded-xl md:grid-cols-2 shadow-indigo-800 dark:shadow-indigo-400 dark:border-indigo-500"
+                    class="grid min-h-full grid-cols-1 gap-2 p-2 overflow-hidden border-indigo-500 shadow-lg md:border bg-indigo-300/70 dark:bg-indigo-500/70 sm:rounded-none lg:rounded-xl md:grid-cols-2 shadow-indigo-800 dark:shadow-indigo-400 dark:border-indigo-500"
                 >
                     <div
-                        class="p-2 border-2 border-indigo-400 border-dashed shadow-lg shadow-transparent dark:border-indigo-600 group hover:border-indigo-500 hover:border-solid hover:shadow-indigo-600 dark:hover:shadow-indigo-500"
+                        class="p-2 border-2 border-indigo-400 border-dashed shadow-lg shadow-transparent dark:border-indigo-400 group hover:border-indigo-500 hover:border-solid hover:shadow-indigo-600 dark:hover:shadow-indigo-500"
                     >
                         <Participant :participants="participants" />
                     </div>
                     <div
-                        class="p-2 border-2 border-indigo-400 border-dashed shadow-lg shadow-transparent dark:border-indigo-600 group hover:border-indigo-500 hover:border-solid hover:shadow-indigo-600 dark:hover:shadow-indigo-500"
+                        class="p-2 border-2 border-indigo-400 border-dashed shadow-lg shadow-transparent dark:border-indigo-400 group hover:border-indigo-500 hover:border-solid hover:shadow-indigo-600 dark:hover:shadow-indigo-500"
                     >
                         <Maker :makers="makers" />
                     </div>
