915b07b341c18be390da2bcbd05a2c5b8565d629
Moosavi <moosavi238@gmail.com>
2022-01-21 13:23:02 +0330

    echeng input error and cheng location

---
+++ resources/js/component/InputError.vue
@@ -0,0 +1,33 @@
+<template>
+    <span>{{ error }}</span>
+</template>
+
+<script>
+import { computed } from "@vue/reactivity";
+import { useToast } from "vue-toastification";
+
+export default {
+    props: ["message"],
+    setup(props) {
+        const error = computed(() => {
+            if (props.message) {
+                useToast().error(props.message, {
+                    position: "bottom-right",
+                    timeout: 5000,
+                    closeOnClick: true,
+                    pauseOnFocusLoss: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    draggablePercent: 0.6,
+                    showCloseButtonOnHover: false,
+                    hideProgressBar: false,
+                    closeButton: "button",
+                    icon: true,
+                    rtl: false,
+                });
+            }
+        });
+        return { error };
+    },
+};
+</script>
