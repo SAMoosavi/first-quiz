0c5ca75c99bd9a2c269b9ec67fa016acca12d34e
Moosavi <moosavi238@gmail.com>
2022-01-21 22:55:30 +0330

    sorten cod and add disabeled

--- resources/js/component/Input.vue
+++ resources/js/component/Input.vue
@@ -1,9 +1,11 @@
 <template>
     <input
-        class="p-2 text-gray-900 bg-transparent border-indigo-700 rounded-md shadow-sm  shadow-indigo-600 dark:shadow-indigo-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
+        class="p-2 text-gray-900 bg-transparent border-indigo-700 rounded-md shadow-sm disabled:bg-gray-300 disabled:shadow-sm shadow-indigo-600 dark:shadow-indigo-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
-        :type="TYPE"
+        :type="type"
+        :required="required"
+        :disabled="disabled"
     />
 </template>
 
@@ -11,13 +13,12 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    props: ["modelValue", "type"],
-    emits: ["update:modelValue"],
-    setup(props) {
-        const TYPE = props.type ? props.type : "text";
-        return {
-            TYPE,
-        };
+    props: {
+        modelValue: String,
+        type: { default: "text", type: String },
+        disabled: { disabled: Boolean, default: false },
+        required: { required: Boolean, default: true },
     },
+    emits: ["update:modelValue"],
 });
 </script>
