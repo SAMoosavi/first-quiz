daf36d13988dfe2f6b13598959ab27dec845be70
Moosavi <moosavi238@gmail.com>
2022-01-21 17:17:05 +0330

    okey section border

--- resources/js/Jetstream/SectionBorder.vue
+++ resources/js/Jetstream/SectionBorder.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hidden sm:block">
         <div class="py-8">
-            <div class="border-t border-gray-200"></div>
+            <div class="border-t border-indigo-500 shadow dark:border-indigo-200 shadow-indigo-500 dark:shadow-indigo-200"></div>
         </div>
     </div>
 </template>
