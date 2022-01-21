0ecfa274ea0a52bdc6fd9d6d129672e0bc628dbd
Moosavi <moosavi238@gmail.com>
2022-01-21 16:44:45 +0330

    ok uodate password form

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -1,93 +1,148 @@
 <template>
-    <jet-form-section @submitted="updatePassword">
-        <template #title>
-            Update Password
-        </template>
+    <form-section @submitted="updatePassword">
+        <template #title> بروزرسانی رمز عبور </template>
 
         <template #description>
-            Ensure your account is using a long, random password to stay secure.
+            اطمینان حاصل کنید که حساب شما از یک رمز عبور طولانی و تصادفی برای
+            حفظ امنیت استفاده می کند.
         </template>
 
         <template #form>
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="current_password" value="Current Password" />
-                <jet-input id="current_password" type="password" class="mt-1 block w-full" v-model="form.current_password" ref="current_password" autocomplete="current-password" />
-                <jet-input-error :message="form.errors.current_password" class="mt-2" />
+                <Label for="current_password" value="رمز عبور فعلی" />
+                <Input
+                    id="current_password"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.current_password"
+                    ref="current_password"
+                    autocomplete="current-password"
+                />
+                <Input-error
+                    :message="form.errors.current_password"
+                    class="mt-2"
+                />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="password" value="New Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" ref="password" autocomplete="new-password" />
-                <jet-input-error :message="form.errors.password" class="mt-2" />
+                <Label for="password" value="رمز عبور جدید" />
+                <Input
+                    id="password"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password"
+                    ref="password"
+                    autocomplete="new-password"
+                />
+                <Input-error :message="form.errors.password" class="mt-2" />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" autocomplete="new-password" />
-                <jet-input-error :message="form.errors.password_confirmation" class="mt-2" />
+                <Label for="password_confirmation" value="تکرار رمز عبور " />
+                <Input
+                    id="password_confirmation"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password_confirmation"
+                    autocomplete="new-password"
+                />
+                <Input-error
+                    :message="form.errors.password_confirmation"
+                    class="mt-2"
+                />
             </div>
         </template>
 
         <template #actions>
-            <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                Saved.
-            </jet-action-message>
+            <action-message :on="form.recentlySuccessful" class="ml-3">
+                ذخیره.
+            </action-message>
 
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </jet-button>
+            <Button class="mr-4" :loding="loding" :disabled="form.processing">
+                ذخیره
+            </Button>
         </template>
-    </jet-form-section>
+    </form-section>
+    <toast
+        v-for="message in messages"
+        :key="message"
+        :message="message"
+        :type="type"
+    />
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import JetActionMessage from '@/Jetstream/ActionMessage.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetFormSection from '@/Jetstream/FormSection.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetInputError from '@/Jetstream/InputError.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
+import { defineComponent, ref } from "vue";
+import ActionMessage from "@/component/ActionMessage.vue";
+import Button from "@/component/Button.vue";
+import FormSection from "@/component/FormSection.vue";
+import Input from "@/component/Input.vue";
+import InputError from "@/component/InputError.vue";
+import Label from "@/component/Label.vue";
+import { useForm } from "@inertiajs/inertia-vue3";
+import Toast from "@/component/Toast.vue";
 
-    export default defineComponent({
-        components: {
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-        },
+export default defineComponent({
+    components: {
+        ActionMessage,
+        Button,
+        FormSection,
+        Input,
+        InputError,
+        Label,
+        Toast,
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    current_password: '',
-                    password: '',
-                    password_confirmation: '',
-                }),
-            }
-        },
+    setup() {
+        const form = useForm({
+            current_password: "",
+            password: "",
+            password_confirmation: "",
+        });
+        const loding = ref(false);
 
-        methods: {
-            updatePassword() {
-                this.form.put(route('user-password.update'), {
-                    errorBag: 'updatePassword',
+        const type = ref(null);
+        const messages = ref(null);
+
+        function updatePassword() {
+            loding.value = true;
+            if (
+                !form.current_password ||
+                !form.password ||
+                !form.password_confirmation
+            ) {
+                type.value = "error";
+                messages.value = ["تمامی فیلد های ستاره دار را پر کنید"];
+            } else if (form.password_confirmation != form.password) {
+                type.value = "error";
+                messages.value = ["رمز عبور جدید و تکرار آن یکسان نیست"];
+            } else {
+                form.put(route("user-password.update"), {
+                    errorBag: "updatePassword",
                     preserveScroll: true,
-                    onSuccess: () => this.form.reset(),
-                    onError: () => {
-                        if (this.form.errors.password) {
-                            this.form.reset('password', 'password_confirmation')
-                            this.$refs.password.focus()
-                        }
+                    onSuccess: () => {
+                        type.value = "success";
+                        messages.value = ["رمز عبور با موفقیت تغییر کرد"];
+                    },
+                    onError: (errors) => {
+                        type.value = "error";
+                        messages.value = errors;
+                    },
+                    onFinish: () => {
+                        form.reset();
+                    },
+                });
+            }
+            setTimeout(() => (loding.value = false), 200);
+        }
 
-                        if (this.form.errors.current_password) {
-                            this.form.reset('current_password')
-                            this.$refs.current_password.focus()
-                        }
-                    }
-                })
-            },
-        },
-    })
+        return {
+            form,
+            updatePassword,
+            loding,
+            type,
+            messages,
+        };
+    },
+});
 </script>
