50e55ea966538457b642713ea1a4d21cfa3fb8ca
Moosavi <moosavi238@gmail.com>
2022-01-23 15:19:37 +0330

    creat option component

---
+++ resources/js/component/Option.vue
@@ -0,0 +1,16 @@
+<template>
+    <option
+        :value="value"
+        class="bg-indigo-200 dark:bg-indigo-700 text-gray-900 dark:text-gray-200 hover:bg-indigo-400 dark:hover:bg-indigo-500"
+    >
+        <slot />
+    </option>
+</template>
+
+<script>
+export default {
+    props: ["value"],
+};
+</script>
+
+<style></style>
