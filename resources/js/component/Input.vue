b2acafd1a76314f0a359190bc98b528f53bc9e85
Moosavi <moosavi238@gmail.com>
2022-01-23 15:19:13 +0330

    add icon from input

--- resources/js/component/Input.vue
+++ resources/js/component/Input.vue
@@ -1,12 +1,23 @@
 <template>
-    <input
-        class="p-2 text-gray-900 bg-transparent border-indigo-700 rounded-md shadow-sm disabled:bg-gray-300 disabled:shadow-sm shadow-indigo-600 dark:shadow-indigo-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        :type="type"
-        :required="required"
-        :disabled="disabled"
-    />
+    <div
+        class="relative shadow-sm shadow-indigo-600 dark:shadow-indigo-300 p-0 m-0 overflow-hidden rounded-md text-gray-900 bg-transparent border-indigo-700 rounded-md disabled:bg-gray-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
+    >
+        <input
+            class="w-full p-2 text-gray-900 bg-transparent disabled:bg-gray-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
+            :class="{ 'pr-12': icon }"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            :type="type"
+            :required="required"
+            :disabled="disabled"
+        />
+        <div
+            v-if="icon"
+            class="absolute top-0 right-0 p-2 pb-3 text-white bg-indigo-700 dark:text-black dark:bg-indigo-300"
+        >
+            <slot />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -18,6 +29,7 @@ export default defineComponent({
         type: { default: "text", type: String },
         disabled: { disabled: Boolean, default: false },
         required: { required: Boolean, default: true },
+        icon: { icon: Boolean, default: false },
     },
     emits: ["update:modelValue"],
 });
