c3b839c27671e8982de624e33cdf1413f7f0c704
Moosavi <moosavi238@gmail.com>
2022-01-23 15:54:52 +0330

    ok input

--- resources/js/component/Input.vue
+++ resources/js/component/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="relative shadow-sm shadow-indigo-600 dark:shadow-indigo-300 p-0 m-0 overflow-hidden rounded-md text-gray-900 bg-transparent border-indigo-700 rounded-md disabled:bg-gray-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 border-1 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
+        class="relative p-0 m-0 overflow-hidden text-gray-900 bg-transparent border border-indigo-700 rounded-lg shadow-sm shadow-indigo-600 dark:shadow-indigo-300 disabled:bg-gray-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 dark:border-2 dark:border-indigo-300 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
     >
         <input
             class="w-full p-2 text-gray-900 bg-transparent disabled:bg-gray-300 dark:text-white focus:bg-gray-400/30 dark:focus:bg-white/20 focus:ring-2 focus:ring-indigo-600/50 dark:focus:ring-indigo-100/50"
