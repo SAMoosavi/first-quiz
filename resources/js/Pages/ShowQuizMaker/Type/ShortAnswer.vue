48ebe6c252536c3517dcf08cec68ab7ad3554051
Moosavi <moosavi238@gmail.com>
2022-01-28 11:06:44 +0330

    ok short answer in show quiz

---
+++ resources/js/Pages/ShowQuizMaker/Type/ShortAnswer.vue
@@ -0,0 +1,13 @@
+<template>
+<h3>{{question.questions}}</h3>
+    <Input :disabled="true" :required="false" class="w-1/2 my-3 md:w-1/3" />
+</template>
+<script>
+import Input from "@/component/Input.vue";
+export default {
+    components: { Input },
+    props:['question']
+};
+</script>
+
+<style></style>
