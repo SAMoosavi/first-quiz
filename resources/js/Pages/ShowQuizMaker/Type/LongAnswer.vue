4a991faa0601900c0e63dcbe0c48f8f2862db727
Moosavi <moosavi238@gmail.com>
2022-01-28 11:07:51 +0330

    ok long ans in show quiz

---
+++ resources/js/Pages/ShowQuizMaker/Type/LongAnswer.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>{{ question.questions }}</h3>
+    <Textarea :required="false" :disabled="true" />
+</template>
+<script>
+import Textarea from "@/component/Textarea.vue";
+export default {
+    components: { Textarea },
+    props: ["question"],
+};
+</script>
+
+<style></style>
