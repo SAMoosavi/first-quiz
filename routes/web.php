617c20c4ebeaa597954c1feb1c25f085c2da9813
Moosavi <moosavi238@gmail.com>
2022-01-28 10:22:10 +0330

    ok same Rout

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\Quiz;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +18,46 @@ use Inertia\Inertia;
 |
 */
 
+
+/* --------------------------------Api-------------------------------- */
+// Get Time
+Route::get('/get-time', function () {
+    return response(['now' => time()], 200);
+});
+
+/* --------------------------------Route-------------------------------- */
+// ---------Home Page
 Route::get('/', function () {
     return Inertia::render('Home/Home');
 });
+// ---------Dashboard Page
+Route::get('/dashboard', [IndexController::class, 'dashboard'])->middleware(['auth:sanctum', 'verified'])->name('dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+
+// ---------Teacher Quiz
+
+// Create Quiz Page
+Route::get('/create/quiz', [QuizController::class, function () {
+    return Inertia::render('Quiz/CreateQuiz');
+}])->name(('create.quiz'))->middleware(['auth:sanctum', 'verified']);
+
+// Create Quiz Backend
+Route::post('/create/quiz', [QuizController::class, 'store'])->name('store.quiz')->middleware(['auth:sanctum', 'verified']);
+
+// Show Quiz Page
+Route::get('/quiz/{uuid}', function ($uuid) {
+    $quiz = Quiz::where('uuid', '=', $uuid)->get()[0];
+    $quiz->questions;
+    return Inertia::render('ShowQuizMaker/ShowQuiz', ['quiz' => $quiz]);
+})->name('show.quiz')->middleware(['auth:sanctum', 'verified']);
+
+
+
+// ---------Student Quiz
+
+// Show Quiz Page
+Route::get('/show/{uuid}', function ($uuid) {
+    $quiz = Quiz::where('uuid', '=', $uuid)->get()[0];
+    $quiz->questions;
+    return Inertia::render('Student/ShowQuiz', ['quiz' => $quiz]);
+})->name('ans.quiz')->middleware(['auth:sanctum', 'verified']);
