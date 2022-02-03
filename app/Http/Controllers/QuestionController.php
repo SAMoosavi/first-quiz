d582a9c0b24ef17e36965d48dd5b43bf8d50e8b6
Moosavi <moosavi238@gmail.com>
2022-02-03 17:09:38 +0330

    creat update in Question controller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class QuestionController extends Controller
 {
-    //
+    public function update(Question $id, Request $request)
+    {
+        $request->validate([
+            'questions' => "required|string",
+            'option' => 'nullable|array',
+            'answer' => 'nullable|string',
+            'uuid' => 'required|string',
+        ]);
+
+        $id->update([
+            'questions' => $request->questions,
+            'option' => json_encode($request->option),
+            'answer' => json_encode($request->answer),
+        ]);
+
+        return  back()->withInput();
+    }
 }
