788cbfca1e6bd07aa78550b35e55d1b67f9219ba
Moosavi <moosavi238@gmail.com>
2022-02-09 19:24:00 +0330

    okey get participants

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
+use App\Models\StudentQuiz;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,10 +13,11 @@ class IndexController extends Controller
 {
     public function dashboard()
     {
-        $user =Auth::user();
+        $user = Auth::user();
         $makers = $user->quizzesMaker;
-        $participants = $user->quizzesParticipants;
+        $participants =  StudentQuiz::where('user_id', '=', $user->id)->get()->map(function ($ids) {
+            return Quiz::find($ids->quiz_id);
+        });
         return Inertia::render('Dashboard/Dashboard', ['makers' => $makers, 'participants' => $participants]);
-
     }
 }
