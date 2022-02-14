93d96fe23925679eefda5bf37220b0f41ac57a21
Moosavi <moosavi238@gmail.com>
2022-02-14 09:37:55 +0330

    okey show start and end in dashbord

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,15 +8,24 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Date\Date;
 
 class IndexController extends Controller
 {
     public function dashboard()
     {
         $user = Auth::user();
-        $makers = $user->quizzesMaker;
+        $makers = $user->quizzesMaker->map(function ($quiz) {
+            $quiz->start = !!$quiz->start ? (new Date($quiz->start))->toJalali()->format('Y/m/d H:i')  : null;
+            $quiz->end = !!$quiz->end ? (new  Date($quiz->end))->toJalali()->format('Y/m/d H:i')  : null;
+            return $quiz;
+        });
         $participants =  StudentQuiz::where('user_id', '=', $user->id)->get()->map(function ($ids) {
-            return Quiz::find($ids->quiz_id);
+
+            $quiz = Quiz::find($ids->quiz_id);
+            $quiz->start = !!$quiz->start ? (new Date($quiz->start))->toJalali()->format('Y/m/d H:i')  : null;
+            $quiz->end = !!$quiz->end ? (new  Date($quiz->end))->toJalali()->format('Y/m/d H:i')  : null;
+            return $quiz;
         });
         return Inertia::render('Dashboard/Dashboard', ['makers' => $makers, 'participants' => $participants]);
     }
