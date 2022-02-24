1a7068b905b94ed16d46b61f1286fd37375d5d84
Moosavi <moosavi238@gmail.com>
2022-02-24 16:10:31 +0330

    edite update function for update of question and  creat point function

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
 use App\Models\Question;
+use App\Models\StudentQuiz;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
@@ -14,7 +16,7 @@ class QuestionController extends Controller
             'questions' => "required|string",
             'option' => 'nullable|array',
             'answer' => 'nullable|string',
-            'point'=>'required|numeric',
+            'point' => 'required|numeric',
             'uuid' => 'required|string',
         ]);
 
@@ -26,5 +28,23 @@ class QuestionController extends Controller
         ]);
 
         return  back()->withInput();
+    }
+
+    public function point(Request $request)
+    {
+        foreach ($request->all() as $userId => $answers) {
+            $pointQuiz = 0;
+            foreach ($answers['points'] as $questionId => $point) {
+                Answer::where('user_id', '=', $userId)->where('question_id', '=', $questionId)->update([
+                    'point' =>  number_format((float)$point, 2, '.', ''),
+                ]);
+                $pointQuiz += $point;
+            }
+            StudentQuiz::where('user_id', '=', $userId)->where('quiz_id', '=', $answers['point']['id'])->update([
+                'point' => $pointQuiz,
+            ]);
+        }
+
+        return back()->withInput();
     }
 }
